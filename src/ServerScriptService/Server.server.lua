00b8c6cdb40db6b4011e02fb2399ac0f5ed58079
cyrus01337 <cyrus21337@gmail.com>
2024-02-21 05:11:50 +0000

    Cleanup and refactor

--- src/ServerScriptService/Server.server.lua
+++ src/ServerScriptService/Server.server.lua
@@ -1,11 +1,60 @@
 local Players = game:GetService("Players")
 local ServerStorage = game:GetService("ServerStorage")
-print("Hello world!")
 
 local LEADERSTATS_TEMPLATE: Model = ServerStorage.leaderstats
 local BASEPLATE = workspace:WaitForChild("Baseplate") :: Part
 local BRICK: Part = ServerStorage.Brick
 local debounces = {}
+
+local function calculatePositionOnTopOf(part: Part, brickHeight: number)
+	local offset = Vector3.new(
+		math.random(-part.Size.X / 2, part.Size.X / 2),
+		(part.Size.Y / 2) + brickHeight,
+		math.random(-part.Size.X / 2, part.Size.X / 2)
+	)
+
+	return part.Position + offset
+end
+
+local function handleBrickTouched(hit: Part, newBrick: Part)
+	local characterFound = hit:FindFirstAncestorOfClass("Model")
+	local playerFound = Players:GetPlayerFromCharacter(characterFound)
+
+	if not playerFound then
+		return
+	end
+
+	local debounceFound = debounces[playerFound]
+
+	if not debounceFound then
+		debounceFound = 1
+		debounces[playerFound] = 1
+	else
+		debounceFound += 1
+		debounces[playerFound] += 1
+	end
+
+	if debounceFound > 1 then
+		return
+	end
+
+	playerFound.leaderstats.BricksEaten.Value += 1
+	newBrick:Destroy()
+
+	debounces[playerFound] = 0
+end
+
+local function spawnBrickOnto(part: Part)
+	local newBrick = BRICK:Clone()
+	newBrick.Color = Color3.fromRGB(255, 255, 0)
+	newBrick.Position = calculatePositionOnTopOf(part, newBrick.Size.Y)
+
+	newBrick.Touched:Connect(function(hit: Part)
+		handleBrickTouched(hit, newBrick)
+	end)
+
+	newBrick.Parent = workspace
+end
 
 local function onPlayerAdded(player: Player)
 	local leaderstats = LEADERSTATS_TEMPLATE:Clone()
@@ -21,45 +70,7 @@ end
 Players.PlayerAdded:Connect(onPlayerAdded)
 
 while true do
-	-- TODO: break out into function
-	local newBrick = BRICK:Clone()
-	newBrick.Color = Color3.fromRGB(255, 255, 0)
-	newBrick.Position = BASEPLATE.Position
-		+ Vector3.new(
-			math.random(-BASEPLATE.Size.X / 2, BASEPLATE.Size.X / 2),
-			(BASEPLATE.Size.Y / 2) + newBrick.Size.Y,
-			math.random(-BASEPLATE.Size.X / 2, BASEPLATE.Size.X / 2)
-		)
-
-	newBrick.Touched:Connect(function(hit: Part)
-		local characterFound = hit:FindFirstAncestorOfClass("Model")
-		local playerFound = Players:GetPlayerFromCharacter(characterFound)
-
-		if not playerFound then
-			return
-		end
-
-		local debounceFound = debounces[playerFound]
-
-		if not debounceFound then
-			debounceFound = 1
-			debounces[playerFound] = 1
-		else
-			debounceFound += 1
-			debounces[playerFound] += 1
-		end
-
-		if debounceFound > 1 then
-			return
-		end
-
-		playerFound.leaderstats.BricksEaten.Value += 1
-		newBrick:Destroy()
-
-		debounces[playerFound] = 0
-	end)
-
-	newBrick.Parent = workspace
+	spawnBrickOnto(BASEPLATE)
 
 	task.wait(1 / 60)
 end
