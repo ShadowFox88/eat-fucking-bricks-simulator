2990290c69624e28376e0d544c2036b18ae6f058
cyrus01337 <cyrus21337@gmail.com>
2024-02-22 05:30:48 +0000

    Pan on right click + drag

--- src/StarterPlayer/StarterPlayerScripts/PlayerModule/Camera.lua
+++ src/StarterPlayer/StarterPlayerScripts/PlayerModule/Camera.lua
@@ -1,20 +1,67 @@
 --!strict
+local ContextActionService = game:GetService("ContextActionService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local RunService = game:GetService("RunService")
+local UserInputService = game:GetService("UserInputService")
 
 local CustomPlayer = require(ReplicatedStorage.CustomPlayer)
 
+local MOUSE_SENSITIVITY = 1 / 10
 local player = CustomPlayer.get()
 local playerCamera = workspace.CurrentCamera
+local accumulatingDelta = Vector2.zero
+-- local rotationalOffset = CFrame.identity
 local Camera = {}
 
 local function trackPlayerCharacter()
-	local origin = player.Character.HumanoidRootPart.CFrame * CFrame.new(0, player.Character.Torso.Size.Y + 2, 5)
+	local rotationalOffset = CFrame.fromOrientation(
+		-math.rad(accumulatingDelta.Y * MOUSE_SENSITIVITY),
+		-math.rad(accumulatingDelta.X * MOUSE_SENSITIVITY),
+		0
+	)
+	local positionalOffset = CFrame.new(0, player.Character.Torso.Size.Y + 2, 5)
+	local origin = player.Character.HumanoidRootPart.CFrame * rotationalOffset * positionalOffset
 	playerCamera.CFrame = CFrame.new(origin.Position, player.Character.HumanoidRootPart.Position)
+end
+
+local function togglePanning(state: Enum.UserInputState)
+	if state == Enum.UserInputState.Begin then
+		UserInputService.MouseBehavior = Enum.MouseBehavior.LockCurrentPosition
+	elseif state == Enum.UserInputState.End then
+		UserInputService.MouseBehavior = Enum.MouseBehavior.Default
+	end
+end
+
+local function pan(action: string, state: Enum.UserInputState, input: InputObject)
+	if
+		state ~= Enum.UserInputState.Begin
+		and state ~= Enum.UserInputState.Change
+		and state ~= Enum.UserInputState.End
+	then
+		return
+	end
+
+	if input.UserInputType == Enum.UserInputType.MouseButton2 then
+		togglePanning(state)
+	elseif
+		input.UserInputType == Enum.UserInputType.MouseMovement
+		and UserInputService.MouseBehavior == Enum.MouseBehavior.LockCurrentPosition
+	then
+		accumulatingDelta += UserInputService:GetMouseDelta()
+	end
+
+	return Enum.ContextActionResult.Pass
 end
 
 local function bindCameraToPlayerCharacter()
 	RunService.RenderStepped:Connect(trackPlayerCharacter)
+	ContextActionService:BindAction(
+		"pan",
+		pan,
+		false,
+		Enum.UserInputType.MouseButton2,
+		Enum.UserInputType.MouseMovement
+	)
 end
 
 function Camera.init()
