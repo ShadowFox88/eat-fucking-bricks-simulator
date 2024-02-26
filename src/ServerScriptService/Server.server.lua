c87e38d8747a931e27d2dbd4b7d98bc24fb3eb1d
cyrus01337 <cyrus21337@gmail.com>
2024-02-26 09:40:51 +0000

    Clarify casting workspace variable to any

--- src/ServerScriptService/Server.server.lua
+++ src/ServerScriptService/Server.server.lua
@@ -11,65 +11,67 @@ local BRICK = ServerStorage.Brick
 local debounces = {}
 
 local function calculatePositionOnTopOf(part: Part, brickHeight: number)
-	local offset = Vector3.new(
-		math.random(-part.Size.X / 2, part.Size.X / 2),
-		(part.Size.Y / 2) + brickHeight,
-		math.random(-part.Size.X / 2, part.Size.X / 2)
-	)
+    local offset = Vector3.new(
+        math.random(-part.Size.X / 2, part.Size.X / 2),
+        (part.Size.Y / 2) + brickHeight,
+        math.random(-part.Size.X / 2, part.Size.X / 2)
+    )
 
-	return part.Position + offset
+    return part.Position + offset
 end
 
 local function handleBrickTouched(hit: BasePart, newBrick: Part)
-	local characterFound = hit:FindFirstAncestorOfClass("Model")
+    local characterFound = hit:FindFirstAncestorOfClass("Model")
 
-	if not characterFound then
-		return
-	end
+    if not characterFound then
+        return
+    end
 
-	local playerFound = Players:GetPlayerFromCharacter(characterFound) :: CustomPlayer.Type?
+    local playerFound = Players:GetPlayerFromCharacter(characterFound) :: CustomPlayer.Type?
 
-	if not (playerFound and Leaderstats.find(playerFound)) then
-		return
-	end
+    if not (playerFound and Leaderstats.find(playerFound)) then
+        return
+    end
 
-	local debounceFound = debounces[playerFound]
+    local debounceFound = debounces[playerFound]
 
-	if not debounceFound then
-		debounceFound = 1
-		debounces[playerFound] = 1
-	else
-		debounceFound += 1
-		debounces[playerFound] += 1
-	end
+    if not debounceFound then
+        debounceFound = 1
+        debounces[playerFound] = 1
+    else
+        debounceFound += 1
+        debounces[playerFound] += 1
+    end
 
-	if debounceFound > 1 then
-		return
-	end
+    if debounceFound > 1 then
+        return
+    end
 
-	playerFound.leaderstats.BricksEaten.Value += 1
-	newBrick:Destroy()
+    playerFound.leaderstats.BricksEaten.Value += 1
+    newBrick:Destroy()
 
-	debounces[playerFound] = 0
+    debounces[playerFound] = 0
 end
 
 local function spawnBrickOnto(part: Part)
-	local newBrick = BRICK:Clone()
-	newBrick.Color = Color3.fromRGB(255, 255, 0)
-	newBrick.Position = calculatePositionOnTopOf(part, newBrick.Size.Y)
+    local newBrick = BRICK:Clone()
+    newBrick.Color = Color3.fromRGB(255, 255, 0)
+    newBrick.Position = calculatePositionOnTopOf(part, newBrick.Size.Y)
 
-	newBrick.Touched:Connect(function(hit: BasePart)
-		handleBrickTouched(hit, newBrick)
-	end)
+    newBrick.Touched:Connect(function(hit: BasePart)
+        handleBrickTouched(hit, newBrick)
+    end)
 
-	-- TODO: Remove any type
-	newBrick.Parent = workspace :: any
+    -- without casting to any, workspace is seen as the original parent of
+    -- newBrick, ServerStorage - this causes an unavoidable type error that can
+    -- only be remedied by the following (as far as im aware)
+    newBrick.Parent = workspace :: any
 end
 
 Players.PlayerAdded:Connect(Leaderstats.init)
 
 while true do
-	spawnBrickOnto(BASEPLATE)
+    spawnBrickOnto(BASEPLATE)
 
-	task.wait(1 / 60)
+    task.wait(1 / 60)
 end
