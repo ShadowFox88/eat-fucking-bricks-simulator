53cf91c366b24a0db8c0434a3e8c1ea821dee6a6
Vahin Mehra <vahin@vahin.dev>
2024-11-21 23:25:35 +0000

    IT'S FUCKING MARIO!!!!!!!!!!!!!!!

--- src/StarterPlayer/StarterPlayerScripts/PlayerModule/Camera.lua
+++ src/StarterPlayer/StarterPlayerScripts/PlayerModule/Camera.lua
@@ -1,132 +1,58 @@
---!strict
-local ContextActionService = game:GetService("ContextActionService")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local RunService = game:GetService("RunService")
-local UserGameSettings = UserSettings():GetService("UserGameSettings")
-local UserInputService = game:GetService("UserInputService")
-
-local CustomPlayer = require(ReplicatedStorage.CustomPlayer)
-local CustomRaycastParams = require(ReplicatedStorage.CustomRaycastParams)
-local SharedState = require(script.Parent.SharedState)
-
-local ZOOM_IN_FACTOR = 1
-local ZOOM_OUT_FACTOR = -1
-local player = CustomPlayer.get()
-local playerCamera = workspace.CurrentCamera
-local Camera = {}
-
-local function getInstanceAheadOf(cameraCFrame: CFrame): Instance?
-    local parameters = CustomRaycastParams.new({
-        FilterDescendantsInstances = { player.Character.Torso },
-        FilterType = Enum.RaycastFilterType.Include,
-        IgnoreWater = true,
-    })
-    local collision = workspace:Raycast(cameraCFrame.Position, cameraCFrame.LookVector, parameters)
-
-    return if collision then collision.Instance else nil
-end
-
-local function trackPlayerCharacter(context: SharedState.CameraContext)
-    local orientation = CFrame.fromOrientation(
-        -math.rad(context.PanDelta.Y * UserGameSettings.MouseSensitivity),
-        -math.rad(context.PanDelta.X * UserGameSettings.MouseSensitivity),
-        0
-    )
-    local positionalOffset = CFrame.new(0, player.Character.Torso.Size.Y + 2, 5)
-    local zoomOffset = CFrame.new(0, 0, context.ZoomFactor)
-    local origin = CFrame.new(player.Character.HumanoidRootPart.Position) * orientation * positionalOffset
-    local newCameraCFrame = CFrame.new(origin.Position, player.Character.HumanoidRootPart.Position) * zoomOffset
-    local instanceAhead = getInstanceAheadOf(newCameraCFrame)
-    local cameraToCharacterDirection = playerCamera.CFrame.Position - player.Character.HumanoidRootPart.Position
-    local studsFromCharacter = math.round(cameraToCharacterDirection.Magnitude)
-    local inFirstPerson = studsFromCharacter == 0
-
-    if instanceAhead and instanceAhead:IsDescendantOf(player.Character) then
-        local insideCharacterCFrame = CFrame.new(
-            player.Character.HumanoidRootPart.Position,
-            player.Character.HumanoidRootPart.Position + player.Character.HumanoidRootPart.CFrame.LookVector
-        )
-        newCameraCFrame = insideCharacterCFrame
-    end
-
-    playerCamera.CFrame = newCameraCFrame
-
-    if inFirstPerson ~= context.InFirstPerson then
-        context.InFirstPerson = inFirstPerson
-    end
-end
-
-local function togglePanning(state: Enum.UserInputState)
-    if state == Enum.UserInputState.Begin then
-        UserInputService.MouseBehavior = Enum.MouseBehavior.LockCurrentPosition
-    elseif state == Enum.UserInputState.End then
-        UserInputService.MouseBehavior = Enum.MouseBehavior.Default
-    end
-end
-
-local function pan(action: string, state: Enum.UserInputState, input: InputObject)
-    if
-        state ~= Enum.UserInputState.Begin
-        and state ~= Enum.UserInputState.Change
-        and state ~= Enum.UserInputState.End
-    then
-        return Enum.ContextActionResult.Pass
-    end
-
-    if input.UserInputType == Enum.UserInputType.MouseButton2 then
-        togglePanning(state)
-    elseif
-        input.UserInputType == Enum.UserInputType.MouseMovement
-        and UserInputService.MouseBehavior == Enum.MouseBehavior.LockCurrentPosition
-    then
-        local mouseDelta = UserInputService:GetMouseDelta()
-        SharedState.CameraContext.PanDelta = Vector2.new(
-            SharedState.CameraContext.PanDelta.X + mouseDelta.X,
-            math.clamp(SharedState.CameraContext.PanDelta.Y + mouseDelta.Y, -110, 45)
-        )
-    end
-
-    return Enum.ContextActionResult.Pass
-end
-
-local function zoom(action: string, state: Enum.UserInputState, input: InputObject)
-    if state ~= Enum.UserInputState.Change then
-        return Enum.ContextActionResult.Pass
-    end
-
-    local scrollingUp = input.Position.Z == 1
-
-    if scrollingUp and not SharedState.CameraContext.InFirstPerson then
-        SharedState.CameraContext.ZoomFactor -= ZOOM_IN_FACTOR
-    elseif not scrollingUp then
-        SharedState.CameraContext.ZoomFactor -= ZOOM_OUT_FACTOR
-    end
-
-    return Enum.ContextActionResult.Pass
-end
-
-local function bindCameraToPlayerCharacter(context: SharedState.CameraContext)
-    RunService.RenderStepped:Connect(function()
-        trackPlayerCharacter(context)
-    end)
-    ContextActionService:BindAction(
-        "pan",
-        pan,
-        false,
-        Enum.UserInputType.MouseButton2,
-        Enum.UserInputType.MouseMovement
-    )
-    ContextActionService:BindAction("zoom", zoom, false, Enum.UserInputType.MouseWheel)
-end
-
-function Camera.init()
-    if player.Character then
-        bindCameraToPlayerCharacter(SharedState.CameraContext)
-    end
-
-    player.CharacterAdded:Connect(function()
-        bindCameraToPlayerCharacter(SharedState.CameraContext)
-    end)
-end
-
-return Camera
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⡶⠟⠋⠉⣉⡤⠶⠒⠚⣛⠿⢿⣷⢦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡴⠛⠁⠀⠀⢀⡾⠉⣶⣤⣀⡀⣿⣿⣦⣈⠻⣮⠻⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠋⠀⠀⠀⠀⠀⣾⠀⠀⣿⢻⡿⣿⣣⣿⠻⣿⣷⣜⣷⠀⠙⢧⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⡋⠀⠀⠀⠀⠀⠀⠀⢻⡀⣄⣿⠚⣷⠀⠉⠛⠀⠈⠻⣿⣿⣀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠿⠛⠷⣶⣶⠶⣶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡾⠏⠀⠀⠀⠀⠀⠀⠀⠀⠈⢳⣌⢿⣤⣿⡀⣀⣠⣤⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⢀⣾⡏⠀⠀⢀⣿⠁⠀⠀⢸⡿⠻⠶⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠳⠾⢟⡿⣩⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⢀⣾⠟⠁⠀⠀⠀⠀⠀⠀⠐⢿⡁⠀⠀⢠⡟⠛⢷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⣫⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡗⠀⠀⠀⠀⣾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡇⠀⠀⢸⠇⠀⠀⣽⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⢫⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⢸⣟⠆⠀⠤⠄⠀⠀⠤⠤⠤⢤⡾⣟⣒⣢⣿⣦⡀⢰⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡞⣵⣿⣿⣿⣿⣿⡿⠟⠛⠉⠁⠀⠀⠀⢿⣿⠟⠛⢿⣿⣿⣿⣿⡿⠃⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⢀⣀⠀⠀⠀⠀⠹⠀⠈⠁⠀⠉⠳⢨⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⢏⣾⣿⣿⣿⣿⣿⣷⣶⣄⡀⠀⠀⠀⠀⠀⠸⠿⣠⠤⣬⡙⣿⣿⣟⣀⡀⠀⠀⠀⠀⢸⣷⠀⠀⠀⠀⠀⠈⠛⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⣹⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⣡⣿⣿⣿⣿⣿⠿⠛⠛⠛⠛⠿⠆⠀⠀⠀⠀⠀⢸⣥⢶⣮⡿⠞⠛⠋⠉⠛⠻⢶⣄⡀⠀⢻⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⣰⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡟⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠟⣴⣿⣿⢿⣿⡟⠁⠀⢀⡴⠖⠒⠒⣦⠀⠀⠀⠀⠀⠈⠿⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⡄⣾⠟⠓⠒⠒⠦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣧⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣋⣾⣿⡟⠁⠘⠛⠀⠀⢠⠏⠀⠀⡾⣻⣯⣽⣦⠀⠀⢀⡖⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣷⣿⠷⠦⣤⣀⡀⠀⠀⠀⠀⠀⠀⠛⠦⣤⣾⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣩⣾⡿⠋⠀⠀⠀⠀⠀⠀⢸⡀⠀⠀⣇⣿⣷⣿⣿⣧⢀⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠏⠀⠀⠀⠈⠉⠙⠓⠶⣤⣀⣀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⢷⠀⠀⠹⣞⢿⣿⣿⡟⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢙⣿⣿⠻⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠆⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢳⡄⠀⠈⠳⠮⠭⢾⡟⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠃⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⠻⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠲⠤⠤⣴⣞⣫⣤⣿⣦⣤⣀⡀⠀⠀⠀⠀⣀⣤⣶⣿⣿⣿⡿⣧⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣤⣤⣤⣤⣾⣿⣿⣿⣿⣟⠀⠀⠉⢻⡳⣄⠀⠀⠀⠀⠀⣿⣿⠛⠛⠛⠋⠉⠉⠁⠀⠀⠀⢙⣻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠁⢿⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⢀⣶⣿⡄⠀⠀⠀⠀⢿⣿⣷⣀⣠⣴⣶⣦⣀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣟⡭⠛⣿⡄⠀⠀⣼⠀⠀⠀⠀⠀⠀⠀⣾⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⡿⠟⠛⠛⠛⠻⢿⣿⡀⠘⠻⣿⡟⠀⠀⠀⠀⠀⠀⠉⠛⠿⠿⠛⢿⣿⣿⣿⣿⠟⢉⣩⡿⠟⢋⣀⣤⣶⣿⣿⣿⠀⠀⣿⠀⠀⠀⠀⠀⠀⢠⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣿⠏⠀⣤⠖⠒⠒⠦⣄⠻⣷⡄⠀⣘⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠓⠆⠀⠀⢻⣥⣴⣾⣿⣿⣿⣿⣿⣿⠃⠀⣸⡇⠀⠀⠀⠀⠀⠀⣼⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣯⠀⠚⠁⠀⠀⠀⠀⠈⠳⣝⣿⣾⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⡿⠃⡰⣰⣿⣿⡀⠀⠀⠀⠀⢰⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠺⡎⣭⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⡤⠞⣱⣿⣿⣿⡇⠀⠀⠀⢠⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠿⠈⠳⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠀⠀⣴⣿⣿⣿⣿⡇⠀⠀⢀⣾⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⣨⡃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣾⣿⣿⣿⣿⣿⣧⠀⠀⢸⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⣷⣶⣄⣠⠀⠀⢀⣠⣶⣿⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣴⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣤⡀⢸⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠛⠛⠛⣿⣿⣿⣿⣿⣿⣷⣦⣤⣀⣀⣤⣤⣴⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠟⠛⠉⠻⣯⡻⢿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣤⣤⢿⡿⠿⠿⠟⠛⠉⠉⠉⠉⠉⠉⠛⠛⠿⣽⡿⣉⠉⠉⠉⠉⠛⠷⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⢮⣿⠇⠙⠻⣷⠶⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣤⠶⠞⠛⠛⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣝⢦⣄⠀⠀⠀⠀⠉⢻⢦⣄⣠⣤⡤⠶⢛⠋⠁⠀⠀⠀⠈⢳⣄⠙⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⡶⠶⠶⢶⣟⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⣌⠳⣄⠀⠀⣠⠶⠛⠛⠳⣦⡀⠁⠀⠀⠀⠀⠀⠀⠀⠀⢙⢦⣌⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣤⣤⣤⣶⡶⣶⡶⠟⠁⠀⠀⠀⠀⠈⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣧⠈⠳⢸⡇⠀⠀⠀⠀⠈⢿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⢹⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣀⣤⠶⢿⠿⠟⡋⢀⣠⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢳⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣽⡀⠘⢉⢷⣄⠀⠀⠀⣠⡿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠹⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢀⣴⠟⠋⠁⠀⠀⠀⠀⠉⠉⠁⡴⠒⣰⡆⠀⠀⠀⠀⢀⡀⠀⠀⠻⡄⠀⠀⠀⠀⠀⠀⢀⣀⡀⠀⣀⣀⣀⣠⣤⣶⣾⣿⣶⣤⡄⠀⠀⠀⠀⠀⠀⢰⡟⠁⠀⠈⢷⣍⣛⣛⣛⣻⡾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠁⠀⠀⠀⠀⠀⠹⡄⠀⠀⢻⡀⣀⣤⣴⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠉⠀⠀⠀⠀⠀⠀⢠⡿⠁⠀⠀⠀⠀⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠇⢰⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡄⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠆⠀⠀⠀⠀⠀⣠⡴⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⡆⠀⠀⠀⡏⠀⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⢠⣿⣿⣿⣿⠿⠟⠛⠛⠉⠉⠉⠀⠀⠀⠈⠛⢿⣿⣿⠇⣀⣤⡴⠞⠋⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣀⡘⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣷⠀⠀⠀⡇⠀⠀⢸⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣁⣴⡿⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣛⣻⠻⣇⠁⠀⠂⠈⠀⠀⣰⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⢿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠘⢷⣤⠀⢷⠀⠀⢸⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡟⠃⠀⠘⣧⠀⠀⠀⢀⡼⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣆⠀⠀⠀⠀⠀⠀⣀⣠⣤⣤⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⢿⣆⠀⠀⠀⠸⡇⠀⠀⠀⠀⠀⡷⣦⡀⠀⠀⠀⠀⠀⢸⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣷⠀⠀⠉⠹⡆⣠⡶⠋⢀⣤⠶⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⡀⠀⣠⡶⠟⠋⠉⠀⠀⠀⠈⠉⠛⠷⣤⡀⠀⠀⠀
+⠀⠀⠀⠙⠷⣦⠀⠀⠷⠀⠀⠀⠀⠀⠃⠈⣧⠀⠀⠀⠀⢠⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡄⠀⠀⠀⢻⡅⠀⠈⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢹⡾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠳⣄⠀⠀
+⠀⠀⠀⠀⠀⠙⠷⣦⣄⡀⠀⠀⠀⠀⢀⣠⡇⠀⠀⠀⢀⣾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣧⠀⠀⠀⠀⠻⣆⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠚⠷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠃⣰⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣆⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣝⠒⠀⠚⠉⠚⠀⠀⢀⣀⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢷⡄⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠁⣰⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⠶⠞⠿⣆
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠷⣦⣤⣀⣀⣠⣤⡿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣆⠀⠀⠀⠀⠀⠀⢠⡟⠀⢀⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠞⠉⣀⣤⠀⠐⢺
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣧⡀⠀⠀⠀⢀⡟⠀⠀⣸⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠛⠁⣠⡾⠋⠀⠀⣀⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣶⣿⣿⣿⣿⣿⣿⣷⣦⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣷⡀⠀⠀⡾⠀⠀⢠⡟⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⠀⣠⡞⠋⠀⠀⠀⣰⡿⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣞⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡖⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢷⡄⣸⠃⠀⠀⣾⠁⠀⠀⠀⠀⠀⠀⠀⣠⡟⠁⢀⡾⠋⠀⠀⠀⢀⣾⠟⠁⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢺⢻⣿⣿⡿⣿⠏⠙⠉⠻⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⢠⡴⣫⡶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣇⠀⠀⣸⠇⠀⠀⠀⠀⠀⠀⢠⡾⠋⠀⣴⠏⠀⠀⠀⠀⣰⡿⠁⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⢸⣿⡟⠀⣿⠀⠀⠀⠀⠈⠻⠛⠻⠿⣿⡆⠀⠀⠀⠀⢠⡟⣸⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠇⠙⠷⣶⡟⠀⠀⠀⠀⠀⠀⣰⠟⠀⣠⡞⠁⠀⠀⢀⣠⡾⠋⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣸⣿⡷⠄⣿⠀⣄⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⢀⡟⣰⡟⠀⠀⠀⠀⠀⠀⠀⢀⣆⠀⣼⠇⠀⠀⢰⡟⠀⠀⠀⠀⠀⣠⣾⣋⡠⠾⠋⠀⠀⣤⡶⠞⠋⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⡇⠀⢹⡆⢹⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡟⣱⡟⠀⠀⠀⠀⠀⠀⠀⢠⡾⠘⢿⡇⠀⠀⢠⡟⠀⠀⠀⠀⠀⢀⡿⠛⢧⣤⠒⠆⠁⢠⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⡇⠀⠀⢿⡈⣧⠀⠀⠀⠀⠀⠀⡤⠴⢖⣋⣥⠾⠋⠀⠀⠀⠀⠀⠀⢀⣴⠟⠀⠀⠈⢷⣀⡆⣿⠁⠀⠀⠀⣀⣰⡟⠀⣠⡟⠁⠀⠀⢠⣾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠘⢷⣈⢃⣤⣤⣤⡤⠴⠶⠚⠛⠉⠀⠀⠀⠀⠀⠀⠀⣠⡴⠋⠁⠀⠀⠀⠀⠀⠉⠋⢻⡄⣤⣴⣾⣿⠏⠀⣰⠏⠀⠀⠀⣰⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣼⡇⠀⠀⠀⠈⢿⣄⠹⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣴⠖⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⡟⠀⣼⠃⠀⠀⣰⠞⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣽⡇⠀⠀⠀⠀⠀⠙⢷⣌⡙⠲⠤⣤⣤⣤⣶⠞⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠷⣴⡇⣀⣶⠞⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠛⠶⠶⠶⠞⢻⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⡿⢿⣧⣀⠀⠀⠀⠀⢀⣠⣴⡞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
--- src/StarterPlayer/StarterPlayerScripts/PlayerModule/Movement.lua
+++ src/StarterPlayer/StarterPlayerScripts/PlayerModule/Movement.lua
@@ -1,173 +1,58 @@
---!strict
-local ContextActionService = game:GetService("ContextActionService")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local RunService = game:GetService("RunService")
-
-local Context = require(ReplicatedStorage.Utils.Context)
-local CustomPlayer = require(ReplicatedStorage.CustomPlayer)
-local CustomRaycastParams = require(ReplicatedStorage.CustomRaycastParams)
-local Types = require(ReplicatedStorage.Utils.Types)
-
-local player = CustomPlayer.get()
-local Movement = {}
-
-type UnitVector = Vector3
-type ActionHandler = (string, Enum.UserInputState, InputObject) -> Enum.ContextActionResult
-type OffsetGeneratorProperties = {
-    Negate: boolean,
-}
-type OffsetGenerator = (OffsetGeneratorProperties?) -> UnitVector
-export type DirectionCallbacks = Types.Record<string, OffsetGenerator>
-type ContextDefaults = {
-    ActivatedKeys: Types.Array<string>,
-    Callback: ActionHandler,
-    DirectionCallbacks: DirectionCallbacks,
-    FallingVelocity: Vector3,
-    MovementVelocity: Vector3,
-    OffsetCallbacks: Types.Array<OffsetGenerator>,
-}
-type MovementContext = Context.Type<ContextDefaults>
-
-local function applyGravity(context: MovementContext, delta: number)
-    local playerTorsoUnderside = player.Character.Torso.Position - Vector3.new(0, player.Character.Torso.Size.Y / 2)
-    local directlyBelow = Vector3.new(0, -1 / 10, 0)
-    local parameters = CustomRaycastParams.new({
-        FilterDescendantsInstances = { workspace.Baseplate, workspace.SpawnLocation },
-        FilterType = Enum.RaycastFilterType.Include,
-        IgnoreWater = true,
-    })
-    local collision = workspace:Raycast(playerTorsoUnderside, directlyBelow, parameters)
-
-    if collision then
-        context.FallingVelocity = Vector3.zero
-
-        return
-    end
-
-    context.FallingVelocity += Vector3.new(0, workspace.Gravity * delta, 0)
-end
-
-local function extractKeyCodesFrom(directions: DirectionCallbacks): Types.Array<Enum.KeyCode>
-    local keyCodes = {}
-
-    for name, _ in directions do
-        local success, keyCodeFound: Enum.KeyCode | string = pcall(function()
-            -- even with the pcall present, dynamically accessing this enum
-            -- causes a type error, so we have to cast to any - blame my lsp
-            return (Enum.KeyCode :: any)[name]
-        end)
-
-        if not success and typeof(keyCodeFound) == "string" then
-            local errorMessage = keyCodeFound
-
-            error(errorMessage)
-        end
-
-        table.insert(keyCodes, keyCodeFound)
-    end
-
-    return keyCodes
-end
-
-local function adjustOrientation(context: MovementContext)
-    local isMoving = context.MovementVelocity ~= Vector3.zero
-
-    if (isMoving and not player.Character.Turning.Enabled) or (not isMoving and player.Character.Turning.Enabled) then
-        player.Character.Turning.Enabled = isMoving
-    end
-
-    player.Character.Turning.CFrame = CFrame.new(
-        player.Character.HumanoidRootPart.Position,
-        player.Character.HumanoidRootPart.Position + context.MovementVelocity
-    )
-end
-
-local function processMovement(context: MovementContext, delta: number)
-    local newMovementVelocity = Vector3.zero
-
-    for _, key in context.ActivatedKeys do
-        local calculateOffsetCallback = context.DirectionCallbacks[key]
-
-        if calculateOffsetCallback == nil then
-            error(`Invalid direction callback for input {key}`)
-        end
-
-        local offset = calculateOffsetCallback()
-        newMovementVelocity += offset
-    end
-
-    local activatedKeysCount = #context.ActivatedKeys
-    local atStandstill = newMovementVelocity == Vector3.zero
-
-    if activatedKeysCount == 0 or atStandstill then
-        newMovementVelocity = Vector3.zero
-
-        if not atStandstill then
-            newMovementVelocity = Vector3.zero
-        end
-    else
-        newMovementVelocity = newMovementVelocity.Unit * player.Character.Humanoid.WalkSpeed
-    end
-
-    if context.FallingVelocity ~= Vector3.zero then
-        player.Character.Movement.VectorVelocity = newMovementVelocity - context.FallingVelocity
-
-        return
-    end
-
-    context.MovementVelocity = newMovementVelocity
-    player.Character.Movement.VectorVelocity = newMovementVelocity
-end
-
-local function bindMovementToPlayerCharacter(context: MovementContext)
-    local keys = extractKeyCodesFrom(context.DirectionCallbacks)
-
-    ContextActionService:BindAction("Movement", context.Callback, false, table.unpack(keys))
-    RunService.Stepped:Connect(function(_, delta: number)
-        applyGravity(context, delta)
-        processMovement(context, delta)
-        adjustOrientation(context)
-    end)
-end
-
-function Movement.init(directions: DirectionCallbacks, callback: ActionHandler?)
-    local activatedKeys: Types.Array<string> = {}
-    local movementVelocity = Vector3.zero
-
-    if callback == nil then
-        local function handleMovementDefault(action: string, state: Enum.UserInputState, input: InputObject)
-            local keyName = input.KeyCode.Name
-
-            if state == Enum.UserInputState.Begin then
-                table.insert(activatedKeys, keyName)
-            else
-                local keyAtIndex = table.find(activatedKeys, keyName)
-
-                table.remove(activatedKeys, keyAtIndex)
-            end
-
-            return Enum.ContextActionResult.Pass
-        end
-
-        callback = handleMovementDefault
-    end
-
-    local context: ContextDefaults = Context.create({
-        ActivatedKeys = activatedKeys,
-        Callback = callback :: ActionHandler,
-        DirectionCallbacks = directions,
-        FallingVelocity = Vector3.zero,
-        MovementVelocity = movementVelocity,
-        OffsetCallbacks = {},
-    })
-
-    if player.Character then
-        bindMovementToPlayerCharacter(context)
-    end
-
-    player.CharacterAdded:Connect(function()
-        bindMovementToPlayerCharacter(context)
-    end)
-end
-
-return Movement
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⡶⠟⠋⠉⣉⡤⠶⠒⠚⣛⠿⢿⣷⢦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡴⠛⠁⠀⠀⢀⡾⠉⣶⣤⣀⡀⣿⣿⣦⣈⠻⣮⠻⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠋⠀⠀⠀⠀⠀⣾⠀⠀⣿⢻⡿⣿⣣⣿⠻⣿⣷⣜⣷⠀⠙⢧⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⡋⠀⠀⠀⠀⠀⠀⠀⢻⡀⣄⣿⠚⣷⠀⠉⠛⠀⠈⠻⣿⣿⣀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠿⠛⠷⣶⣶⠶⣶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡾⠏⠀⠀⠀⠀⠀⠀⠀⠀⠈⢳⣌⢿⣤⣿⡀⣀⣠⣤⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⢀⣾⡏⠀⠀⢀⣿⠁⠀⠀⢸⡿⠻⠶⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠳⠾⢟⡿⣩⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⢀⣾⠟⠁⠀⠀⠀⠀⠀⠀⠐⢿⡁⠀⠀⢠⡟⠛⢷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⣫⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡗⠀⠀⠀⠀⣾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡇⠀⠀⢸⠇⠀⠀⣽⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⢫⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⢸⣟⠆⠀⠤⠄⠀⠀⠤⠤⠤⢤⡾⣟⣒⣢⣿⣦⡀⢰⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡞⣵⣿⣿⣿⣿⣿⡿⠟⠛⠉⠁⠀⠀⠀⢿⣿⠟⠛⢿⣿⣿⣿⣿⡿⠃⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⢀⣀⠀⠀⠀⠀⠹⠀⠈⠁⠀⠉⠳⢨⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⢏⣾⣿⣿⣿⣿⣿⣷⣶⣄⡀⠀⠀⠀⠀⠀⠸⠿⣠⠤⣬⡙⣿⣿⣟⣀⡀⠀⠀⠀⠀⢸⣷⠀⠀⠀⠀⠀⠈⠛⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⣹⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⣡⣿⣿⣿⣿⣿⠿⠛⠛⠛⠛⠿⠆⠀⠀⠀⠀⠀⢸⣥⢶⣮⡿⠞⠛⠋⠉⠛⠻⢶⣄⡀⠀⢻⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⣰⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡟⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠟⣴⣿⣿⢿⣿⡟⠁⠀⢀⡴⠖⠒⠒⣦⠀⠀⠀⠀⠀⠈⠿⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⡄⣾⠟⠓⠒⠒⠦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣧⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣋⣾⣿⡟⠁⠘⠛⠀⠀⢠⠏⠀⠀⡾⣻⣯⣽⣦⠀⠀⢀⡖⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣷⣿⠷⠦⣤⣀⡀⠀⠀⠀⠀⠀⠀⠛⠦⣤⣾⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣩⣾⡿⠋⠀⠀⠀⠀⠀⠀⢸⡀⠀⠀⣇⣿⣷⣿⣿⣧⢀⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠏⠀⠀⠀⠈⠉⠙⠓⠶⣤⣀⣀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⢷⠀⠀⠹⣞⢿⣿⣿⡟⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢙⣿⣿⠻⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠆⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢳⡄⠀⠈⠳⠮⠭⢾⡟⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠃⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⠻⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠲⠤⠤⣴⣞⣫⣤⣿⣦⣤⣀⡀⠀⠀⠀⠀⣀⣤⣶⣿⣿⣿⡿⣧⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣤⣤⣤⣤⣾⣿⣿⣿⣿⣟⠀⠀⠉⢻⡳⣄⠀⠀⠀⠀⠀⣿⣿⠛⠛⠛⠋⠉⠉⠁⠀⠀⠀⢙⣻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠁⢿⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⢀⣶⣿⡄⠀⠀⠀⠀⢿⣿⣷⣀⣠⣴⣶⣦⣀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣟⡭⠛⣿⡄⠀⠀⣼⠀⠀⠀⠀⠀⠀⠀⣾⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⡿⠟⠛⠛⠛⠻⢿⣿⡀⠘⠻⣿⡟⠀⠀⠀⠀⠀⠀⠉⠛⠿⠿⠛⢿⣿⣿⣿⣿⠟⢉⣩⡿⠟⢋⣀⣤⣶⣿⣿⣿⠀⠀⣿⠀⠀⠀⠀⠀⠀⢠⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣿⠏⠀⣤⠖⠒⠒⠦⣄⠻⣷⡄⠀⣘⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠓⠆⠀⠀⢻⣥⣴⣾⣿⣿⣿⣿⣿⣿⠃⠀⣸⡇⠀⠀⠀⠀⠀⠀⣼⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣯⠀⠚⠁⠀⠀⠀⠀⠈⠳⣝⣿⣾⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⡿⠃⡰⣰⣿⣿⡀⠀⠀⠀⠀⢰⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠺⡎⣭⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⡤⠞⣱⣿⣿⣿⡇⠀⠀⠀⢠⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠿⠈⠳⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠀⠀⣴⣿⣿⣿⣿⡇⠀⠀⢀⣾⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⣨⡃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣾⣿⣿⣿⣿⣿⣧⠀⠀⢸⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⣷⣶⣄⣠⠀⠀⢀⣠⣶⣿⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣴⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣤⡀⢸⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠛⠛⠛⣿⣿⣿⣿⣿⣿⣷⣦⣤⣀⣀⣤⣤⣴⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠟⠛⠉⠻⣯⡻⢿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣤⣤⢿⡿⠿⠿⠟⠛⠉⠉⠉⠉⠉⠉⠛⠛⠿⣽⡿⣉⠉⠉⠉⠉⠛⠷⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⢮⣿⠇⠙⠻⣷⠶⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣤⠶⠞⠛⠛⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣝⢦⣄⠀⠀⠀⠀⠉⢻⢦⣄⣠⣤⡤⠶⢛⠋⠁⠀⠀⠀⠈⢳⣄⠙⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⡶⠶⠶⢶⣟⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⣌⠳⣄⠀⠀⣠⠶⠛⠛⠳⣦⡀⠁⠀⠀⠀⠀⠀⠀⠀⠀⢙⢦⣌⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣤⣤⣤⣶⡶⣶⡶⠟⠁⠀⠀⠀⠀⠈⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣧⠈⠳⢸⡇⠀⠀⠀⠀⠈⢿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⢹⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣀⣤⠶⢿⠿⠟⡋⢀⣠⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢳⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣽⡀⠘⢉⢷⣄⠀⠀⠀⣠⡿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠹⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢀⣴⠟⠋⠁⠀⠀⠀⠀⠉⠉⠁⡴⠒⣰⡆⠀⠀⠀⠀⢀⡀⠀⠀⠻⡄⠀⠀⠀⠀⠀⠀⢀⣀⡀⠀⣀⣀⣀⣠⣤⣶⣾⣿⣶⣤⡄⠀⠀⠀⠀⠀⠀⢰⡟⠁⠀⠈⢷⣍⣛⣛⣛⣻⡾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠁⠀⠀⠀⠀⠀⠹⡄⠀⠀⢻⡀⣀⣤⣴⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠉⠀⠀⠀⠀⠀⠀⢠⡿⠁⠀⠀⠀⠀⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠇⢰⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡄⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠆⠀⠀⠀⠀⠀⣠⡴⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⡆⠀⠀⠀⡏⠀⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⢠⣿⣿⣿⣿⠿⠟⠛⠛⠉⠉⠉⠀⠀⠀⠈⠛⢿⣿⣿⠇⣀⣤⡴⠞⠋⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣀⡘⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣷⠀⠀⠀⡇⠀⠀⢸⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣁⣴⡿⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣛⣻⠻⣇⠁⠀⠂⠈⠀⠀⣰⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⢿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠘⢷⣤⠀⢷⠀⠀⢸⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡟⠃⠀⠘⣧⠀⠀⠀⢀⡼⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣆⠀⠀⠀⠀⠀⠀⣀⣠⣤⣤⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⢿⣆⠀⠀⠀⠸⡇⠀⠀⠀⠀⠀⡷⣦⡀⠀⠀⠀⠀⠀⢸⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣷⠀⠀⠉⠹⡆⣠⡶⠋⢀⣤⠶⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⡀⠀⣠⡶⠟⠋⠉⠀⠀⠀⠈⠉⠛⠷⣤⡀⠀⠀⠀
+⠀⠀⠀⠙⠷⣦⠀⠀⠷⠀⠀⠀⠀⠀⠃⠈⣧⠀⠀⠀⠀⢠⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡄⠀⠀⠀⢻⡅⠀⠈⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢹⡾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠳⣄⠀⠀
+⠀⠀⠀⠀⠀⠙⠷⣦⣄⡀⠀⠀⠀⠀⢀⣠⡇⠀⠀⠀⢀⣾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣧⠀⠀⠀⠀⠻⣆⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠚⠷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠃⣰⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣆⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣝⠒⠀⠚⠉⠚⠀⠀⢀⣀⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢷⡄⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠁⣰⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⠶⠞⠿⣆
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠷⣦⣤⣀⣀⣠⣤⡿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣆⠀⠀⠀⠀⠀⠀⢠⡟⠀⢀⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠞⠉⣀⣤⠀⠐⢺
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣧⡀⠀⠀⠀⢀⡟⠀⠀⣸⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠛⠁⣠⡾⠋⠀⠀⣀⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣶⣿⣿⣿⣿⣿⣿⣷⣦⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣷⡀⠀⠀⡾⠀⠀⢠⡟⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⠀⣠⡞⠋⠀⠀⠀⣰⡿⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣞⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡖⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢷⡄⣸⠃⠀⠀⣾⠁⠀⠀⠀⠀⠀⠀⠀⣠⡟⠁⢀⡾⠋⠀⠀⠀⢀⣾⠟⠁⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢺⢻⣿⣿⡿⣿⠏⠙⠉⠻⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⢠⡴⣫⡶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣇⠀⠀⣸⠇⠀⠀⠀⠀⠀⠀⢠⡾⠋⠀⣴⠏⠀⠀⠀⠀⣰⡿⠁⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⢸⣿⡟⠀⣿⠀⠀⠀⠀⠈⠻⠛⠻⠿⣿⡆⠀⠀⠀⠀⢠⡟⣸⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠇⠙⠷⣶⡟⠀⠀⠀⠀⠀⠀⣰⠟⠀⣠⡞⠁⠀⠀⢀⣠⡾⠋⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣸⣿⡷⠄⣿⠀⣄⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⢀⡟⣰⡟⠀⠀⠀⠀⠀⠀⠀⢀⣆⠀⣼⠇⠀⠀⢰⡟⠀⠀⠀⠀⠀⣠⣾⣋⡠⠾⠋⠀⠀⣤⡶⠞⠋⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⡇⠀⢹⡆⢹⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡟⣱⡟⠀⠀⠀⠀⠀⠀⠀⢠⡾⠘⢿⡇⠀⠀⢠⡟⠀⠀⠀⠀⠀⢀⡿⠛⢧⣤⠒⠆⠁⢠⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⡇⠀⠀⢿⡈⣧⠀⠀⠀⠀⠀⠀⡤⠴⢖⣋⣥⠾⠋⠀⠀⠀⠀⠀⠀⢀⣴⠟⠀⠀⠈⢷⣀⡆⣿⠁⠀⠀⠀⣀⣰⡟⠀⣠⡟⠁⠀⠀⢠⣾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠘⢷⣈⢃⣤⣤⣤⡤⠴⠶⠚⠛⠉⠀⠀⠀⠀⠀⠀⠀⣠⡴⠋⠁⠀⠀⠀⠀⠀⠉⠋⢻⡄⣤⣴⣾⣿⠏⠀⣰⠏⠀⠀⠀⣰⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣼⡇⠀⠀⠀⠈⢿⣄⠹⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣴⠖⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⡟⠀⣼⠃⠀⠀⣰⠞⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣽⡇⠀⠀⠀⠀⠀⠙⢷⣌⡙⠲⠤⣤⣤⣤⣶⠞⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠷⣴⡇⣀⣶⠞⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠛⠶⠶⠶⠞⢻⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⡿⢿⣧⣀⠀⠀⠀⠀⢀⣠⣴⡞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
